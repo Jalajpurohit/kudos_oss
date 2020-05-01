8e0fd72dc366ff95d5ec2d1281dc4b7ffb34d267
Jalaj Purohit <Jalajpurohit09@gmail.com>
2020-05-01 21:04:20 +0530

    Updated code

--- app/kudo/schema.py
+++ app/kudo/schema.py
@@ -1,14 +1,11 @@
 from marshmallow import Schema, fields
 
-
 class GithubRepoSchema(Schema):
     id = fields.Int(required=True)
-    repo_name = fields.Str()
-    full_name = fields.Str()
+    full_name  = fields.Str()
     language = fields.Str()
     description = fields.Str()
-    repo_url = fields.URL()
-
+    html_url = fields.URL()
 
 class KudoSchema(GithubRepoSchema):
     user_id = fields.Email(required=True)
